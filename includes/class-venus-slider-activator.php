b8f3afc7c79892cecad5c7f654d1fba2ed033379
Felipe Borges <felipetecacc@gmail.com>
2025-05-13 11:40:04 -0300

    feat: Created the class that runs during plugin activation

--- includes/class-venus-slider-activator.php
+++ includes/class-venus-slider-activator.php
@@ -20,17 +20,71 @@
  * @subpackage Venus_Slider/includes
  * @author     Felipe Borges <felipetecacc@gmail.com>
  */
-class Venus_Slider_Activator {
+if ( ! class_exists( 'Venus_Slider_Activator' ) ):
+	
+	class VenusSliderActivator {
+		
+		protected static $instance = null;
 
-	/**
-	 * Short Description. (use period)
-	 *
-	 * Long Description.
-	 *
-	 * @since    1.0.0
-	 */
-	public static function activate() {
+		/**
+		 * Ensures only one instance of this class is loaded or can be loaded.
+		 * 
+		 * @return VenusSliderActivator
+		 */
+		public static function init() {
+			if ( is_null( self::$instance ) ) {
+				self::$instance = new self();
+			}
 
+			return self::$instance;
+		}
+
+		public function __construct() {
+			add_action( 'venus_slider_activation', array( $this, 'activate' ) );
+		}
+
+		/**
+		 * Script that should load upon plugin activation
+		 */
+		public function activate() {
+			$version = get_option( 'venus_slider_version' );
+
+			if ( $version == false ) {
+				$this->update_meta();
+			}
+
+			// Add plugin version to database
+			update_option( 'venus_slider_version', VENUS_SLIDER_VERSION );
+
+			// Flush the rewrite rules on activation
+			flush_rewrite_rules();
+		}
+
+		/**
+		 * Update meta version 1.0.0
+		 */
+		public function update_meta() {
+			$carousels = get_posts( array(
+				'post_type'   => 'venus-slider',
+				'post_status' => 'any', 
+			) );
+
+			if ( count( $carousels ) > 0 ) {
+				foreach ( $carousels as $carousel ) {
+					$id             = $carousel->ID;
+					$_items_desktop = get_post_meta( $id, '_items', true );
+					$_lazy_load     = get_post_meta( $id, '_lazy_load_image', true );
+					$_lazy_load     = $_lazy_load == 'on' ? 'on' : 'off';
+
+					update_post_meta( $id, '_lazy_load_image', $_lazy_load );
+					update_post_meta( $id, '_items_desktop', $_items_desktop );
+					update_post_meta( $id, '_slide_type', 'image-carousel' );
+					update_post_meta( $id, '_video_width', '560' );
+					update_post_meta( $id, '_video_width',  '315');
+				}
+			}
+		}
 	}
+endif;
 
-}
+VenusSliderActivator::init();
