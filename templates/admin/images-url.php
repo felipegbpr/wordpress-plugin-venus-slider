a4d85ccb8010ca179bf9d55dcf26868ccaebc4fa
Felipe Borges <felipetecacc@gmail.com>
2025-05-17 15:21:28 -0300

    feat: Added images url admin template

---
+++ templates/admin/images-url.php
@@ -0,0 +1,104 @@
+<?php 
+$_images_urls = get_post_meta( $post->ID, '_images_urls', true );
+$btn_text     = $_images_urls ? 'Edit Urls' : 'Add URLs';
+?>
+
+<div data-id="open" id="section_url_images_settings" class="shapla-toggle shapla-toggle--stroke"
+    style="display: <?php echo $slide_type != 'image-carousel-url' ? 'none' : 'block'; ?>">
+    <span class="shapla-toggle-title">
+        <?php _e( 'URL Images', 'venus-slider' ) ?>
+    </span>
+    <div class="shapla-toggle-inner">
+        <div class="shapla-toggle-content">
+            <?php 
+            $this->form->images_url( array(
+                'id'   => '_images_urls',
+                'name' => __( 'Images URLs', 'venus-slider' ),
+                'desc' => __( 'Enter external images URLs.', 'venus-slider' ),
+            ) );
+            ?>
+        </div>
+    </div>
+</div>
+
+<!-- The Modal -->
+<div id="VenusSliderModal" class="venus_slider-modal">
+
+    <!-- Modal content --> 
+    <div class="venus_slider-modal-content">
+        <div class="venus_slider-modal-header">
+            <span class="venus_slider-close">&times;</span>
+            <?php _e( 'Image Carousel - from URL', 'venus-slider' ); ?>
+        </div>
+        <div class="venus_slider-modal-body">
+            <div id="venus_slider_form" class="venus_slider-form">
+                <?php if ( is_array( $_images_urls ) ): foreach ( $_images_urls as $image ): ?>
+                <div class="venus_slider-fields">
+                    <label class="setting">
+                        <span class="name"><?php _e( 'URL', 'venus-slider' ); ?></span>
+                        <input type="url" name="_images_urls[url][]" value="<?php echo $image['url']; ?>" 
+                            autocomplete="off"/>
+                    </label>
+                    <label class="setting">
+                        <span class="name"><?php _e( 'Title', 'venus-slider' ); ?></span>
+                        <input type="text" name="_images_urls[title][]" value="<?php echo $image['title']; ?>" 
+                            autocomplete="off"/>
+                    </label>
+                    <label class="setting">
+                        <span class="name"><?php _e( 'Caption', 'venus-slider' ); ?></span>
+                        <textarea name="_images_urls[caption][]"><?php echo $image['caption']; ?></textarea>
+                    </label>
+                    <label class="setting">
+                        <span class="name"><?php _e( 'Alt Text', 'venus-slider' ); ?></span>
+                        <input type="text" name="_images_urls[alt][]" value="<?php echo $image['alt']; ?>" 
+                            autocomplete="off"/>
+                    </label>
+                    <label class="setting">
+                        <span class="name"><?php _e( 'Link To URL', 'venus-slider' ); ?></span>
+                        <input type="text" name="_images_urls[link_url][]" value="<?php echo $image['link_url']; ?>" 
+                            autocomplete="off"/>
+                    </label>
+                    <div class="actions">
+                        <span><span class="dashicons dashicons-move"></span></span>
+                        <span class="add_row"><span class="dashicons dashicons-plus-alt"></span></span>
+                        <span class="delete_row"><span class="dashicons dashicons-trash"></span></span>
+                    </div>
+                </div>    
+                <?php endforeach; else: ?>   
+                <div class="venus_slider-fields">
+                    <label class="setting">
+                        <span class="name"><?php _e( 'URL', 'venus-slider' ); ?></span>
+                        <input type="url" name="_images_urls[url][]" value="" />
+                    </label>
+                    <label class="setting">
+                        <span class="name"><?php _e( 'Title', 'venus-slider' ); ?></span>
+                        <input type="text" name="_images_urls[title][]" value="" />
+                    </label>
+                    <label class="setting">
+                        <span class="name"><?php _e( 'Caption', 'venus-slider' ); ?></span>
+                        <textarea name="_images_urls[caption][]"></textarea>
+                    </label>
+                    <label class="setting">
+                        <span class="name"><?php _e( 'Alt Text', 'venus-slider' ); ?></span>
+                        <input type="text" name="_images_urls[alt][]" value="" />
+                    </label>
+                    <label class="setting">
+                        <span class="name"><?php _e( 'Link To URL', 'venus-slider' ); ?></span>
+                        <input type="text" name="_images_urls[link_url][]" value="" />
+                    </label>
+                    <div class="actions">
+                        <span><span class="dashicons dashicons-move"></span></span>
+                        <span class="add_row"><span class="dashicons dashicons-plus-alt"></span></span>
+                        <span class="delete_row"><span class="dashicons dashicons-trash"></span></span>
+                    </div>
+                </div> 
+                <?php endif; ?>        
+            </div>
+        </div>
+        <div class="venus_slider-modal-footer">
+            <button class="button button-primary" id="save_carousel_images_urls">
+                <?php _e( 'Save', 'venus-slider' ); ?>
+            </button>
+        </div>
+    </div>           
+</div>
