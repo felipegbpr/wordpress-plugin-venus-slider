f6959d87e0a54468469dc3980de2b08955c86f66
Felipe Borges <felipetecacc@gmail.com>
2025-05-20 16:16:39 -0300

    feat: Implemented i18n slider class

--- includes/class-venus-slider-i18n.php
+++ includes/class-venus-slider-i18n.php
@@ -24,24 +24,42 @@
  * @subpackage Venus_Slider/includes
  * @author     Felipe Borges <felipetecacc@gmail.com>
  */
-class Venus_Slider_i18n {
 
+if ( ! class_exists( 'Venus_Slider_i18n' ) ):
 
-	/**
-	 * Load the plugin text domain for translation.
-	 *
-	 * @since    1.0.0
-	 */
-	public function load_plugin_textdomain() {
+	class Venus_Slider_i18n {
+		protected static $instance = null;
+		protected $plugin_name = 'venus-slider';
+		
+		/**
+		 * Ensures only one instance of this class is loaded or can be loaded.
+		 *
+		 * @return Venus_Slider_i18n
+	     */
+		public static function init() {
+			if ( is_null( self::$instance ) ) {
+				self::$instance = new self();
+			}
 
-		load_plugin_textdomain(
-			'venus-slider',
-			false,
-			dirname( dirname( plugin_basename( __FILE__ ) ) ) . '/languages/'
-		);
+			return self::$instance;
+		}
 
+		public function __construct() {
+			add_action( 'init', array( $this, 'load_textdomain' ) );
+		}
+
+		/**
+		 * Load plugin textdomain
+		 */
+		public function load_textdomain() {
+			$locale_file = sprintf( '%1$s-%2$s.mo', 'venus-slider', get_locale() );
+			$global_file = join( DIRECTORY_SEPARATOR, [ WP_LANG_DIR, 'venus-slider', $locale_file ] );
+
+			// Look in global /wp-content/languages/venus-slider folder
+			if ( file_exists( $global_file ) ) {
+				load_textdomain( $this->plugin_name, $global_file );
+			}
+		}
 	}
 
-
-
-}
+endif;
