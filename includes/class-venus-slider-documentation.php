3689c30dcd89c3f43a2887efb89d3dc0aecf158a
Felipe Borges <felipetecacc@gmail.com>
2025-05-20 14:32:07 -0300

    feat: Implemented documentation slider class

---
+++ includes/class-venus-slider-documentation.php
@@ -0,0 +1,48 @@
+<?php
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+if ( ! class_exists( 'VenusSliderDocumentation' ) ) {
+
+    class VenusSliderDocumentation {
+
+        protected static $instance = null;
+
+        /**
+         * Ensures only one instance of this class is loaded or can be loaded.
+         * 
+         * @return VenusSliderDocumentation
+         */
+        public static function init() {
+            if ( is_null( self::$instance ) ) {
+                self::$instance = new self();
+            }
+
+            return self::$instance; 
+        }
+
+        public function __construct() {
+            add_action( 'admin_menu', array( $this, 'admin_menu' ) );
+        }
+
+        public function admin_menu() {
+            add_submenu_page(
+                'edit.php?post_type=carousels',
+                'Documentation',
+                'Documentation',
+                'manage_options',
+                'venus-slider-documentation',
+                array( $this, 'submenu_page_callback' )
+            );
+        }
+
+        public function submenu_page_callback() {
+            include_once VENUS_SLIDER_TEMPLATES . '/documentation.php';
+        }
+    }
+
+}
+
+
