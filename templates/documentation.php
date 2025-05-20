86685755c792f5c460a88e03354ae88075479c08
Felipe Borges <felipetecacc@gmail.com>
2025-05-20 15:49:28 -0300

    feat: Implemented documentation slider template

---
+++ templates/documentation.php
@@ -0,0 +1,180 @@
+<?php
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+?>
+<style type="tex/css">
+    .iframe_wrapper {
+        position: relative;
+        padding-bottom: 56.25%; /* height / width * 100 */
+        padding-top: 25px;
+        height: 0;
+    }
+
+    .iframe_wrapper > iframe {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+    }
+</style>
+<div class="wrap">
+    <h1 class="wp-heading"><?php _e( 'Venus Slider Documentation', 'venus-slider' ); ?></h1>
+    <div id="poststuff">
+        <div id="post-body" class="metabox-holder columns-2">
+            <div id="post-body-content">
+                <div class="postbox">
+                    <div class="inside">
+                        <table class="form-table">
+                            <tr>
+                                <th scope="row">
+                                    <label>Images Carousel</label>
+                                    <p class="description">Image carousel using gallery images</p>
+                                </th>
+                                <td>
+                                    <div class="iframe_wrapper">
+                                        <iframe 
+                                            width="1280" 
+                                            height="720" 
+                                            src="https://youtu.be/MtkvZQF5QxI?si=bgRXLJsbO7Xj4rKt" 
+                                            frameborder="0"
+                                            allowfullscreen
+                                        >
+                                        </iframe>
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <th scope="row">
+                                    <label>Images Carousel</label>
+                                    <p class="description">Image carousel using custom URLs</p>
+                                </th>
+                                <td>
+                                    <div class="iframe_wrapper">
+                                        <iframe 
+                                            width="1280" 
+                                            height="720" 
+                                            src="https://youtu.be/QYfVlfsMsAE?si=0ySJy43IywoVFLo8" 
+                                            frameborder="0"
+                                            allowfullscreen
+                                        >
+                                        </iframe>
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <th scope="row">
+                                    <label>Posts Carousel</label>
+                                </th>
+                                <td>
+                                    <div class="iframe_wrapper">
+                                        <iframe 
+                                            width="1280" 
+                                            height="720" 
+                                            src="https://youtu.be/DaaCThv_NNM?si=s-FJcS-A8b4OZCgs" 
+                                            frameborder="0"
+                                            allowfullscreen
+                                        >
+                                        </iframe>
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <th scope="row">
+                                    <label>WooCommerce Products Carousel</label>
+                                </th>
+                                <td>
+                                    <div class="iframe_wrapper">
+                                        <iframe 
+                                            width="1280" 
+                                            height="720" 
+                                            src="https://youtu.be/s4uBOKqMMsE?si=XAT8d_SvIDAoonED" 
+                                            frameborder="0"
+                                            allowfullscreen
+                                        >
+                                        </iframe>
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <th scope="row">
+                                    <label>In Widget Areas</label>
+                                </th>
+                                <td>
+                                    <div class="iframe_wrapper">
+                                        <iframe 
+                                            width="1280" 
+                                            height="720" 
+                                            src="https://youtu.be/sgC9ISgPpNQ?si=a0vgmaGJatNCBPDd" 
+                                            frameborder="0"
+                                            allowfullscreen
+                                        >
+                                        </iframe>
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <th scope="row">
+                                    <label>With Page Builder by SiteOrigin</label>
+                                </th>
+                                <td>
+                                    <div class="iframe_wrapper">
+                                        <iframe 
+                                            width="1280" 
+                                            height="720" 
+                                            src="https://youtu.be/u1zS3ypuDMY?si=7hGQDG5BlUsYU2_t" 
+                                            frameborder="0"
+                                            allowfullscreen
+                                        >
+                                        </iframe>
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <th scope="row">
+                                    <label>With WPBakery Visual Composer</label>
+                                </th>
+                                <td>
+                                    <div class="iframe_wrapper">
+                                        <iframe 
+                                            width="1280" 
+                                            height="720" 
+                                            src="https://youtu.be/gLHV7GUnna0?si=LcZ7N6xMdSjqwHOt" 
+                                            frameborder="0"
+                                            allowfullscreen
+                                        >
+                                        </iframe>
+                                    </div>
+                                </td>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
+            </div> <!-- #post-body-content -->
+
+            <div id="postbox-container-1" class="postbox-container">
+                <div class="postbox">
+                    <div class="inside">
+                        <p>
+                            <strong>
+                                If you like this plugin or if you make money using this or if you want to help me to
+                                continue my contribution on open source projects, consider to make a small
+                                donation.
+                            </strong>
+                        </p>
+                        <p style="text-align:center;">
+                            <a href="#" target="_blank">
+                                <img alt="PayPal Donate"
+                                     src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif">
+                            </a>
+                        </p>
+                    </div>
+                </div>
+            </div><!-- #postbox-container-1 -->
+
+        </div><!-- #post-body -->
+    </div><!-- #poststuff -->
+    <br class="clear">
+</div>
