70969fd4fea4ef1fb26b51f93c906435127ba2bd
Felipe Borges <felipetecacc@gmail.com>
2025-05-28 15:52:58 -0300

    feat: Added gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+const livereload = require('gulp-livereload');
+
+gulp.task('scss', function() {
+    gulp.src('./assets/scss/*.scss')
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }).on('error', sass.logError))
+        .pipe(gulp.dest('./assets/css'))
+        .pipe(livereload());
+});
+
+gulp.task('js', function() {
+    gulp.src('./assets/js/admin/*.js')
+        .pipe(concat('admin.js'))
+        .pipe(gulp.dest('./assets/js'))
+        .pipe(concat('admin.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('./assets/js'))
+        .pipe(livereload())
+});
+
+gulp.task('watch', function() {
+    livereload.listen();
+    gulp.watch('./assets/scss/*.scss', ['scss']);
+    gulp.watch('./assets/js/src/*.js', ['js']);
+});
+
+gulp.task('default', ['scss', 'js', 'watch']);
